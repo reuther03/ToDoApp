cb9607003c7ba7f2f602c758104cbdaef2c6b2d8
michaldrozd <michaldrozd03@gmail.com>
2025-07-06 21:27:21 +0200

    Sets up a basic React/TypeScript/Vite client

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+    return
+    <div className="App">
+        <h1>Welcome to the Application</h1>
+        <p>Please log in to continue.</p>
+        {/* You can add your login component here */}
+    </div>
+}
+
+export default App
